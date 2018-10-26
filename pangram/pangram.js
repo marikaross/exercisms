94a453b64df90f60162dcc1242fd0cb427c9bd8a
Marika Ross <marika.ross@gmail.com>
2018-10-26 18:18:55 -0400

    Map out for loop for pangram tests

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -4,13 +4,22 @@ class pangram {
   }
 
   isPangram() {
+    let checkIt = []
     if(!this.words) {
       return false
     }
     let cleaned = this.words.toLowerCase()
+    for (let i = 0, i<= cleaned.length, i++) {
+      //compare to already existing array of alphabet? seems inefficient
+      checkIt.push([i])
+      if(checkIt.includes([i])) {
+        return false
+      } else if (the array does not contain ALL letters a-z) {
+        return false
+      }
+    }
     return true
    }
 
 }
 
-module.exports = pangram;
