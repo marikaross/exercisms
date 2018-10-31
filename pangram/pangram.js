73622aeb34d4f3df847c988020a76dda521e63b8
Marika Ross <marika.ross@gmail.com>
2018-10-31 08:55:38 -0600

    export pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -9,17 +9,18 @@ class pangram {
       return false
     }
     let cleaned = this.words.toLowerCase()
-    for (let i = 0, i<= cleaned.length, i++) {
-      //compare to already existing array of alphabet? seems inefficient
+    for (let i = 0; i <= cleaned.length; i++) {
+      //compare to already existing array of alphabet? 
+      //seems inefficient. but could use a count?
       checkIt.push([i])
-      if(checkIt.includes([i])) {
-        return false
-      } else if (the array does not contain ALL letters a-z) {
+      if (checkIt.includes([i])) {
         return false
       }
-    }
     return true
    }
 
+  }
 }
+
+module.exports = pangram
 
